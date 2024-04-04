9b21e7ab8a5e0dbbaf1d67f9f47758c7b49ccf42
Khalil Riahi <khaliloz9003@gmail.com>
2024-04-04 04:06:06 +0000

    message

--- Tab7ira1/projet/src/app/app.module.ts
+++ Tab7ira1/projet/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SearchComponent } from './header/search/search.component';
+import { UserComponent } from './header/user/user.component';
+import { TitleComponent } from './header/title/title.component';
+import { ContainerComponent } from './container/container.component';
+import { ListBeachesComponent } from './container/list-beaches/list-beaches.component';
+import { BeachComponent } from './container/list-beaches/beach/beach.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    UserComponent,
+    TitleComponent,
+    ContainerComponent,
+    ListBeachesComponent,
+    BeachComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [
     provideClientHydration()
